41f400190906e7eb3af358e4155eb79d608ed6cf
Alexander Gornak <s-kozelsk@yandex.ru>
2024-04-21 11:28:25 +0300

    feat: play with sessions; simple auth + redirects (#2)

---
+++ config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"encoding/json"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	EjConfig
+	DBConfig
+	GinConfig
+}
+
+type EjConfig struct {
+	APIKey    string `json:"EJ_API_KEY"`
+	APISecret string `json:"EJ_API_SECRET"`
+	URL       string `json:"EJ_URL"`
+}
+
+type DBConfig struct {
+	SqlitePath string `json:"SQLITE_PATH"`
+}
+
+type GinConfig struct {
+	Secret string `json:"GIN_SECRET"`
+}
+
+func NewConfig() (*Config, error) {
+	env, err := godotenv.Read()
+	if err != nil {
+		return nil, err
+	}
+	senv, err := json.Marshal(env)
+	if err != nil {
+		return nil, err
+	}
+	var cfg Config
+	if err := json.Unmarshal(senv, &cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
--- controller/login.go
+++ controller/login.go
@@ -3,11 +3,53 @@ package controller
 import (
 	"net/http"
 
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
-func (s *Server) Login(c *gin.Context) {
+type loginForm struct {
+	Login    string `binding:"required" form:"ejLogin"`
+	Password string `binding:"required" form:"ejPassword"`
+}
+
+func (s *Server) LoginGET(c *gin.Context) {
+	session := sessions.Default(c)
+
 	c.HTML(http.StatusOK, "login.html", gin.H{
 		"Title": "Login",
+		"User":  session.Get("user"),
 	})
+}
+
+// TODO: add auth.
+func (s *Server) authUser(_, _ string) bool {
+	return true
+}
+
+func (s *Server) LoginPOST(c *gin.Context) {
+	session := sessions.Default(c)
+
+	var form loginForm
+	if err := c.ShouldBind(&form); err != nil {
+		c.Redirect(http.StatusBadRequest, "/login")
+
+		return
+	}
+
+	if !s.authUser(form.Login, form.Password) {
+		c.Redirect(http.StatusUnauthorized, "/login")
+
+		return
+	}
+
+	session.Set("user", form.Login)
+	_ = session.Save()
+	c.Redirect(http.StatusFound, "/")
+}
+
+func (s *Server) LogoutPOST(c *gin.Context) {
+	session := sessions.Default(c)
+	session.Clear()
+	_ = session.Save()
+	c.Redirect(http.StatusFound, "/")
 }
